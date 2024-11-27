ca5c7226230933262e9238ab2dba1fc4c0142d33
yshagit31 <yshashank024@gmail.com>
2024-11-27 18:35:20 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     <section className="padding-x sm:py-32 py-16 w-full">
       <Subscribe/>
     </section>
-    <section className="padding-x padding-t pb-8">
+    <section className="padding-x padding-t pb-8 bg-black">
       <Footer/>
     </section>
   </main>
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -5,7 +5,7 @@ const Subscribe = () => {
   return (
     <section id='contact-us' className="max-container flex justify-between items-center max-lg:flex-col gap-10">  
     <h2 className='font-bold text-4xl text-center lg:max-w-md leading-[68px] font-palanquin'>Sign Up for <span className='text-coral-red'>Updates</span> & Newsletter</h2> 
-    <div className='lg:max-w-[40%] w-full flex items-center' >
+    <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full' >
          <input type='text' placeholder='subscribe@nike.com' className='input' />
          <div className='flex max-sm:justify-end max-sm:w-full items-center'>
           <Button label="Sign Up" fullWidth></Button>
