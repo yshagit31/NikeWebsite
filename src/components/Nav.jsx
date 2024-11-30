b42316f3870fa4803c0ab22fd7eb7e340d78d449
yshagit31 <yshashank024@gmail.com>
2024-11-30 17:04:07 +0530

    MobNav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,16 @@
 import {  headerLogo} from '../assets/images';
-import {hamburger} from '../assets/icons';
+import {close, hamburger} from '../assets/icons';
 import { navLinks } from '../constants';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [isOpen,setisOpen]=useState(false);
   return (
-   <header className='padding-x py-8 absolute w-full z-10'>
+   <header className='padding-x py-8 absolute w-full max-lg:z-20 lg:z-10'>
     <nav className='flex justify-between items-center max-container'>
-       <a href='/'>  <img src={headerLogo} alt="logo" width={130} height={29}/> </a>
+       <a href='/'>  
+       <img src={headerLogo} alt="logo" width={130} height={29}/> 
+       </a>
        <ul className='flex justify-center items-center flex-1 max-lg:hidden gap-16'>
         {navLinks.map((item)=>(
           <li key={item.label}>
@@ -15,7 +19,19 @@ const Nav = () => {
             </a>
           </li> ))}
        </ul>
-      <img src={hamburger} alt="" className='h-6 w-6 lg:hidden'/>
+      <img src={hamburger} alt="" className={`h-6 w-6 lg:hidden  ${isOpen ? 'pointer-events-none opacity-0' : 'opacity-100'}`}onClick={()=>(setisOpen(true))}/>
+
+       {/* Mobile Navbar */}
+      <ul className={`fixed top-0 right-0 bg-slate-200 bg-opacity-80 w-1/3 h-screen lg:hidden transition-transform duration-500 ease-in-out transform ${isOpen ? 'translate-x-0' :' translate-x-full'} `}>
+      <a className='cursor-pointer' onClick={()=>(setisOpen(false))}> 
+        <img src={close} alt="close icon" className='w-5 h-5 ml-auto mr-4 mt-5' /> </a>
+      {navLinks.map((item)=>(
+          <li key={item.label} >
+            <a href={item.href} className='flex flex-col justify-evenly flex-1 font-montserrat my-10 ml-2 cursor-pointer'>
+            {item.label}
+            </a>
+          </li> ))}
+          </ul>
     </nav>
    </header>
   )
